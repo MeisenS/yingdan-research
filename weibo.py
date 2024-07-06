dfceb3be21358fa020e8f4654c8a4067cc7667da
Meisen Shu <65598232+MeisenS@users.noreply.github.com>
2024-07-06 12:41:02 -0500

    update

--- weibo.py
+++ weibo.py
@@ -5,6 +5,11 @@ from dateutil import tz
 from bs4 import BeautifulSoup
 import re
 import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
 
 def get_tweets_by_date(uuid, span):
     now = datetime.datetime.now(tz=tz.tzlocal())
@@ -27,7 +32,6 @@ def get_tweets_by_date(uuid, span):
     verification = ''
     gender = ''
    
-
     zhiding = 0
 
     for tweet in get_weibo_tweets(tweet_container_id=get_tweet_containerid(uid=uuid), pages=100):
@@ -38,11 +42,17 @@ def get_tweets_by_date(uuid, span):
         if tweet_date > six_months_ago:
             tweet_text = tweet.get('mblog').get('text')
             clean_text = clean_html(tweet_text)
-            tags = extract_hashtags(clean_text)
+            cur_id = tweet.get('mblog').get('id')
 
-            tweets.append(clean_text)
+            if(clean_text[-2::]=='全文'):
+                neww=full_tweet(cur_id)
+                tweets.append(neww)
+                tags = extract_hashtags(neww)
+            else:
+                tweets.append(clean_text)
+                tags = extract_hashtags(clean_text)
             all_tags.append(tags)  
-            ids.append(tweet.get('mblog').get('id'))
+            ids.append(cur_id)
             dates.append(tweet_date)
             likes.append(tweet.get('mblog').get('attitudes_count'))
             comments.append(tweet.get('mblog').get('comments_count'))
@@ -62,9 +72,14 @@ def get_tweets_by_date(uuid, span):
         elif(zhiding >= 5 and tweet_date <= six_months_ago):
             break
 
-
-            
     return ids, dates, tweets, all_tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation
+
+def full_tweet(id):
+    driver = webdriver.Chrome()
+    url = 'https://m.weibo.cn/detail/'+id
+    driver.get(url)
+    text_box = driver.find_element(By.CLASS_NAME, "weibo-text")
+    return(text_box.text)
 
 
 def load_file(file_path):
@@ -96,13 +111,9 @@ def write_to_csv(ids, dates, tweets, tags, likes, comments, reposts, uuid,screen
 
     file_path = 'taiwan_celeb(6months).csv'
     headers = not os.path.exists(file_path)
-
     df_tweets.to_csv(file_path, mode='a', index=False, header = headers)
 
-    
     print(f"Data has been saved for {screenName}")
-
-
 
 def clean_html(html_content):
 
@@ -121,10 +132,6 @@ def main():
         print(f"Processing: {name} with UID: {uuid}")
         ids, dates, tweets, all_tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation = get_tweets_by_date(uuid, span=6)
         write_to_csv(ids, dates, tweets, all_tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation)
-
-if __name__ == '__main__':
-    main()
-
 
 if __name__ == '__main__':
     main()
--- weibo2.py
+++ weibo2.py
@@ -6,25 +6,31 @@ from bs4 import BeautifulSoup
 import re
 import json
 import os
-from openpyxl import load_workbook
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def get_tweets_by_date(uuid, span):
     now = datetime.datetime.now(tz=tz.tzlocal())
     six_months_ago = now - datetime.timedelta(days=span*30)
+    ids = []
+    dates = []
+
     tweets = []
     all_tags = [] 
     likes = []
     comments = []
     reposts = []
+    geolocation = []
 
+    screenName = ''
+    verified_reason = ''
     description = ''
     followers = ''
     statuses = ''
     verification = ''
     gender = ''
-    geolocation = ''
-
+   
     zhiding = 0
 
     for tweet in get_weibo_tweets(tweet_container_id=get_tweet_containerid(uid=uuid), pages=100):
@@ -35,26 +41,45 @@ def get_tweets_by_date(uuid, span):
         if tweet_date > six_months_ago:
             tweet_text = tweet.get('mblog').get('text')
             clean_text = clean_html(tweet_text)
-            tags = extract_hashtags(clean_text)
+            cur_id = tweet.get('mblog').get('id')
 
-            tweets.append(clean_text)
+            if(clean_text[-2::]=='全文'):
+                neww=full_tweet(cur_id)
+                tweets.append(neww)
+                tags = extract_hashtags(neww)
+            else:
+                tweets.append(clean_text)
+                tags = extract_hashtags(clean_text)
             all_tags.append(tags)  
+            ids.append(cur_id)
+            dates.append(tweet_date)
             likes.append(tweet.get('mblog').get('attitudes_count'))
             comments.append(tweet.get('mblog').get('comments_count'))
             reposts.append(tweet.get('mblog').get('reposts_count'))
 
+            screenName = tweet.get('mblog').get('user').get('screen_name')
+            verified_reason = tweet.get('mblog').get('user').get('verified_reason')
             description = tweet.get('mblog').get('user').get('description')
             followers = tweet.get('mblog').get('user').get('followers_count')
             statuses = tweet.get('mblog').get('user').get('statuses_count')
             verification = tweet.get('mblog').get('user').get('verified')
             gender = tweet.get('mblog').get('user').get('gender')
-            geolocation = tweet.get('mblog').get('user').get('region_name')
+            geolocation.append(tweet.get('mblog').get('region_name'))
+            
             if zhiding < 5:
                 zhiding += 1
         elif(zhiding >= 5 and tweet_date <= six_months_ago):
             break
-            
-    return tweets, all_tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation
+
+    return ids, dates, tweets, all_tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation
+
+def full_tweet(id):
+    driver = webdriver.Chrome()
+    url = 'https://m.weibo.cn/detail/'+id
+    driver.get(url)
+    text_box = driver.find_element(By.CLASS_NAME, "weibo-text")
+    return(text_box.text)
+
 
 
 def load_excel_as_json(file_path):
@@ -64,44 +89,34 @@ def load_excel_as_json(file_path):
     return final
 
 
-def write_to_excel(names, sheet, tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation):
-    df = pd.DataFrame({
-        'tweets': tweets,
-        'tags': [', '.join(tag_list) for tag_list in tags],  # Convert list of tags to comma-separated strings
-        'likes':likes,
-        'comments':comments,
-        'reposts':reposts,
-    })
-    
-    df2= pd.DataFrame({
-        'uid': uuid,
-        'description': description,
-        'followers':followers,
-        'statuses':statuses,
-        'verification':verification,
-        'gender':gender,
-        'geolocation':geolocation,
-    },index=[0])
-    file_path = names+'_tweet_texts.xlsx'
-    if os.path.exists(file_path):
-        with pd.ExcelWriter(names+'_tweet_texts.xlsx',mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
-    
-            df2.to_excel(writer, sheet_name=str(sheet)+"_basic_info", index=False)
-            df.to_excel(writer, sheet_name=str(sheet)+"_tweets_within_6months", index=False)
-    else:
-        with pd.ExcelWriter(names+'_tweet_texts.xlsx') as writer:
-    
-            df2.to_excel(writer, sheet_name=str(sheet)+"_basic_info", index=False)
-            df.to_excel(writer, sheet_name=str(sheet)+"_tweets_within_6months", index=False)
-
-    
-    
-    # with pd.ExcelWriter(names+'_tweet_texts.xlsx') as writer:
+def write_to_csv(ids, dates, tweets, tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation):
    
-    #     df2.to_excel(writer, sheet_name=sheet+"_basic_info", index=False)
-    #     df.to_excel(writer, sheet_name=sheet+"_tweets_within_6months", index=False)
+    df_tweets = pd.DataFrame({
+        'id': ids,
+        'date': dates,
+        'geolocation' : geolocation,
+        'tweets': tweets,
+        'tags': [', '.join(tag_list) for tag_list in tags],
+        'likes': likes,
+        'comments': comments,
+        'reposts': reposts,
+        'star_id' : uuid,
+        'screen_name' : screenName,
+        'gender' : gender,
+        'description' : description,
+        'follow_count' : followers,
+        'statuses_count' : statuses,
+        'verification' : verification,
+        'verified_reason' : verified_reason,
+        
+    })
 
-    print(f"{sheet} Data has been saved")
+    file_path = 'celeb_gov(12months).csv'
+    headers = not os.path.exists(file_path)
+
+    df_tweets.to_csv(file_path, mode='a', index=False, header = headers)
+    
+    print(f"Data has been saved for {screenName}")
 
 def write_other_page():
     pass
@@ -120,34 +135,21 @@ def extract_hashtags(text):
 
 def main():
     name_uid_dict = load_excel_as_json('celebrity_media_gov.xlsx')
+    need = []
     for item in name_uid_dict:
-        print(f"Processing: {item['name']}")
-        name = item['name']
-        sheet = item['name']
-        uuid = item['uid_star']
-        tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation= get_tweets_by_date(uuid, span=12)
-        write_to_excel(name, sheet, tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation)
-        if (item['province_media_uid']!=None):
-            uuid = item['province_media_uid']
-            sheet = item['province_media_name']
-            tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation= get_tweets_by_date(uuid, span=12)
-            write_to_excel(name, sheet,tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation)
-        if (item['city_media_uid']!=None):
-            uuid = item['city_media_uid']
-            sheet = item['city_media_name']
-            tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation= get_tweets_by_date(uuid, span=12)
-            write_to_excel(name, sheet,tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation)
-        if (item['province_gov_uid']!=None):
-            uuid = item['province_gov_uid']
-            sheet = item['province_gov_name']
-            tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation= get_tweets_by_date(uuid, span=12)
-            write_to_excel(name, sheet,tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation)
-        if (item['city_gov_uid']!=None):
-            uuid = item['city_gov_uid']
-            sheet = item['city_gov_name']
-            tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation= get_tweets_by_date(uuid, span=12)
-            write_to_excel(name, sheet,tweets, tags, likes, comments, reposts, uuid, description, followers, statuses, verification, gender, geolocation)
-
+        need.append(item['uid_star'])
+        need.append(item['province_media_uid'])
+        need.append(item['city_media_uid'])
+        need.append(item['province_gov_uid'])
+        need.append(item['city_gov_uid'])
+    need=set(need)
+    for item in need:
+        print(f"Processing: {item}")
+        if type(item) == type(2.0):
+            item = int(item)
+        ids, dates, tweets, all_tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation = get_tweets_by_date(item, span=12)
+        write_to_csv(ids, dates, tweets, all_tags, likes, comments, reposts, uuid,screenName, description, followers, statuses, verification,verified_reason, gender, geolocation)
+       
 if __name__ == '__main__':
     main()
 
